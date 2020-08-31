4c0834a3b2cfafcfea6651a9eb6faa384e437826
panjianwei <http://mail.ruigushop.com/>
2020-08-31 17:57:16 +0800

    Revert "feat: 添加代码1283"
    This reverts commit 04b786d9

--- cloudLearn2020/demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ cloudLearn2020/demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class Demo2Application {
 
     public static void main(String[] args) {
-        System.out.println(123);
+        System.out.println(144);
         System.out.println(456);
         System.out.println(789);
         SpringApplication.run(Demo2Application.class, args);
