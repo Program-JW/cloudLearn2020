4aec356b8c90b847d4c7398e870b8a3c55bb4913
panjianwei <http://mail.ruigushop.com/>
2020-08-31 17:48:04 +0800

    feat: 添加代码789

--- cloudLearn2020/demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ cloudLearn2020/demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -11,6 +11,7 @@ public class Demo2Application {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println(456);
+        System.out.println(789);
         SpringApplication.run(Demo2Application.class, args);
     }
 
