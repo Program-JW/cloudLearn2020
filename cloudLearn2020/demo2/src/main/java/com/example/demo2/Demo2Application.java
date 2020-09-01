11a53d6d8d1fff58d1d924914053ced0cb82463d
panjianwei <http://mail.ruigushop.com/>
2020-09-01 09:26:35 +0800

    feat: 2

--- cloudLearn2020/demo2/src/main/java/com/example/demo2/Demo2Application.java
+++ cloudLearn2020/demo2/src/main/java/com/example/demo2/Demo2Application.java
@@ -10,6 +10,7 @@ public class Demo2Application {
 
     public static void main(String[] args) {
         System.out.println("main");//helloworld
+        // 345
         SpringApplication.run(Demo2Application.class, args);
     }
 
